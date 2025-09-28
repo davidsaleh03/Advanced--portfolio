937911fbd0ee65e2bac2213eb60dd1532c18e4dd
David <davidsaleh03@gmail.com>
2025-09-28 00:48:53 -0700

    emailJS complete

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-// template_ae51puj
+// template_1ipwhzs
 // service_aahskhi
 // Icp1xk9l7iS3qDKbA
 
 function contact(event) {
     event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible"
     emailjs
-      .sendForm(
-        'service_aahskhi',
-        'template_ae51puj',
-        event.target,
-        'Icp1xk9l7iS3qDKbA'
-      ).then(() => {
-        console.log('this worked')
-      })
+        .sendForm(
+            'service_aahskhi',
+            'template_1ipwhzs',
+            event.target,
+            'Icp1xk9l7iS3qDKbA'
+        ).then(() => {
+        loading.classList.remove('modal__overlay--visible')
+    success.classList += " modal__overlay--visible"
+        }).catch(() => {
+            loading.classList.remove('modal__overlay--visible')
+            alert(
+                "The email service is temporarily unavaliable. Please contact me directly on davidsaleh03@gmail.com"
+            );
+        }
+    )
 }
